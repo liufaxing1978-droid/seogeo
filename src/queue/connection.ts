c15778ce46074e5f7665c4ca47dd0111f13bb59a
liufaxing1978-droid <liufaxing1978@gmail.com>
2026-08-18 13:47:17 +0800

    feat: add Redis connection factory

---
+++ src/queue/connection.ts
@@ -0,0 +1,6 @@
+import IORedis from 'ioredis';
+import { env } from '../config/env.js';
+
+export function createRedisConnection() {
+  return new IORedis(env.REDIS_URL, { maxRetriesPerRequest: null });
+}
